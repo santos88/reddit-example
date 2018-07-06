448c22d2c16af1d35e51798073a36fd12262e193
Santos Ramon <santos.ramon.brito@gmail.com>
2018-07-06 09:41:45 -0400

    Added basic project structure supporting master detail / split view. Xcode template + MVC

---
+++ RedditClient/RedditClient/Views/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  RedditClient
+//
+//  Created by Santos Ramon on 06-07-18.
+//  Copyright © 2018 Santos Ramon. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
